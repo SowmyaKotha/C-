4f96a98c06104b4702eb517fc62116b06d3705e5
SowmyaKotha <Sowmya.Kotha@Uconn.Edu>
2016-08-10 21:36:34 -0400

    Demo Web API

--- WebAPI/WebAPI/Controllers/MathController.cs
+++ WebAPI/WebAPI/Controllers/MathController.cs
@@ -56,139 +56,139 @@ namespace WebAPI.Controllers
             return "default";
         }
     }
-    class Controller
-    {
-        private string stateOfU;
-        private string stateOfX;
-        private char SignalX;
-        private bool finalState = false;
+    //class Controller
+    //{
+    //    private string stateOfU;
+    //    private string stateOfX;
+    //    private char SignalX;
+    //    private bool finalState = false;
 
-        public Controller()
-        {
-            goStateX();
-            // eventA();
-            //goStateQ();
-
-
-
-        }
-
-        override public string ToString()
-        {
-
-            return "state of U = " + stateOfU + "  state of X = " + stateOfX;
-
-
-        }
-
-        /// 
-
-        /// State entry routines
-        /// 
-
-        private void goStateX()
-        {
-            stateOfU = "X";
-            Console.WriteLine("Entering 'X'=1\n");
-
-        }
-
-        private void goStateY1()
-        {
-            stateOfX = "Y=1";
-            Console.WriteLine("Entering 'Y'=1\n");
-
-
-        }
-        private void goStateY0()
-        {
-            stateOfX = "Y=0";
-            Console.WriteLine("Entering 'Y'=0\n");
-        }
-        private void goStateFinal()
-        {
-            finalState = true;
-        }
-
-        public void eventA()
-        {
-            // goStateY0();
-            if (stateOfU == "X")
-            {
-                Console.WriteLine("Please Enter the initial state");
-                stateOfX = Console.ReadLine();
-                if (stateOfX == "Y=0")
-                {
-                    Console.WriteLine("Please Enter the SignalX value");
-                    SignalX = Convert.ToChar(Console.ReadLine());
-                    if (SignalX == '0')
-                    {
-                        goStateY0();
-                        Console.WriteLine("In 'Y'=0");
-                        Console.WriteLine("In 'Y with SignalX =0");
-                    }
-                    else
-                    {
-                        //Console.WriteLine("Please Enter the SignalX value");
-                        if (SignalX == '1')
-                        {
-
-                            goStateY1();
-                            Console.WriteLine("In 'Y'=1\n with SignalX=1");
-                        }
-                    }
-                }
-                else
-                {
-                    if (stateOfU == "X")
-                    {
-                        Console.WriteLine("Please Enter the initial state");
-                        stateOfX = Console.ReadLine();
-                        if (stateOfX == "Y=1")
-                        {
-                            Console.WriteLine("Please Enter the SignalX value");
-                            SignalX = Convert.ToChar(Console.ReadLine());
-                            if (SignalX == '0')
-                            {
-                                goStateY0();
-                                Console.WriteLine("In 'Y'=0 with SignalX =0");
-                            }
-                            else
-                            {
-                                // Console.WriteLine("Please Enter the SignalX value");
-                                if (SignalX == '1')
-                                {
-                                    goStateY1();
-                                    Console.WriteLine("In 'Y'=1\n with SignalX=1");
-                                }
-                            }
-                        }
-                    }
-                    //goStateP();
-                    //goStateQ();
-
-
-                }
-            }
-        }
+    //    public Controller()
+    //    {
+    //        goStateX();
+    //        // eventA();
+    //        //goStateQ();
 
 
 
+    //    }
 
-        public void eventF()
-        {
-            if (stateOfU == "X")
-            {
-                goStateFinal();
-            }
-            else
-            {
-                Console.WriteLine("Event F ignored");
-            }
-        }
-        public bool IsFinalState()
-        {
-            return finalState;
-        }
-    }
+    //    override public string ToString()
+    //    {
+
+    //        return "state of U = " + stateOfU + "  state of X = " + stateOfX;
+
+
+    //    }
+
+    //    /// 
+
+    //    /// State entry routines
+    //    /// 
+
+    //    private void goStateX()
+    //    {
+    //        stateOfU = "X";
+    //        Console.WriteLine("Entering 'X'=1\n");
+
+    //    }
+
+    //    private void goStateY1()
+    //    {
+    //        stateOfX = "Y=1";
+    //        Console.WriteLine("Entering 'Y'=1\n");
+
+
+    //    }
+    //    private void goStateY0()
+    //    {
+    //        stateOfX = "Y=0";
+    //        Console.WriteLine("Entering 'Y'=0\n");
+    //    }
+    //    private void goStateFinal()
+    //    {
+    //        finalState = true;
+    //    }
+
+    //    public void eventA()
+    //    {
+    //        // goStateY0();
+    //        if (stateOfU == "X")
+    //        {
+    //            Console.WriteLine("Please Enter the initial state");
+    //            stateOfX = Console.ReadLine();
+    //            if (stateOfX == "Y=0")
+    //            {
+    //                Console.WriteLine("Please Enter the SignalX value");
+    //                SignalX = Convert.ToChar(Console.ReadLine());
+    //                if (SignalX == '0')
+    //                {
+    //                    goStateY0();
+    //                    Console.WriteLine("In 'Y'=0");
+    //                    Console.WriteLine("In 'Y with SignalX =0");
+    //                }
+    //                else
+    //                {
+    //                    //Console.WriteLine("Please Enter the SignalX value");
+    //                    if (SignalX == '1')
+    //                    {
+
+    //                        goStateY1();
+    //                        Console.WriteLine("In 'Y'=1\n with SignalX=1");
+    //                    }
+    //                }
+    //            }
+    //            else
+    //            {
+    //                if (stateOfU == "X")
+    //                {
+    //                    Console.WriteLine("Please Enter the initial state");
+    //                    stateOfX = Console.ReadLine();
+    //                    if (stateOfX == "Y=1")
+    //                    {
+    //                        Console.WriteLine("Please Enter the SignalX value");
+    //                        SignalX = Convert.ToChar(Console.ReadLine());
+    //                        if (SignalX == '0')
+    //                        {
+    //                            goStateY0();
+    //                            Console.WriteLine("In 'Y'=0 with SignalX =0");
+    //                        }
+    //                        else
+    //                        {
+    //                            // Console.WriteLine("Please Enter the SignalX value");
+    //                            if (SignalX == '1')
+    //                            {
+    //                                goStateY1();
+    //                                Console.WriteLine("In 'Y'=1\n with SignalX=1");
+    //                            }
+    //                        }
+    //                    }
+    //                }
+    //                //goStateP();
+    //                //goStateQ();
+
+
+    //            }
+    //        }
+    //    }
+
+
+
+
+    //    public void eventF()
+    //    {
+    //        if (stateOfU == "X")
+    //        {
+    //            goStateFinal();
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Event F ignored");
+    //        }
+    //    }
+    //    public bool IsFinalState()
+    //    {
+    //        return finalState;
+    //    }
+    //}
 }
